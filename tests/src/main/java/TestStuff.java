294a8011ca120748a5cb784ecfa7ec21886c3375
Marius <marboravrns@gmail.com>
2017-05-17 14:26:15 +0200

    refactor tests

--- tests/src/main/java/TestStuff.java
+++ tests/src/main/java/TestStuff.java
@@ -4,11 +4,20 @@ public class TestStuff {
 	public static void main(String[] args) {
 
 		AbstractFactoryHardwareSoftware factory = HardwareSoftwareFactoryProducer.getCategoryProduct("Hardware");
+		AbstractFactoryHardwareSoftware factory2 = HardwareSoftwareFactoryProducer.getCategoryProduct("software");
 
-		Hardware cpu = factory.getHardware(HardwareTypes.CHIPSET);
-		Hardware memory = factory.getHardware(HardwareTypes.MEMORY);
-
-		System.out.println(cpu.getClass() + "\n \n" + memory.getClass());
+		Product cpu = (Product) factory.getHardware(HardwareTypes.CHIPSET);
+		Product memory = (Product) factory.getHardware(HardwareTypes.MEMORY);
+		
+		Product cpu2 = (Product) factory.getHardware(HardwareTypes.CHIPSET);
+		
+		Software soft = factory2.getSoftware(SoftwareTypes.OS);
+		
+		System.out.println(cpu2.getClass().getSuperclass() + "\n \n" + memory.getClass() + "\n" + soft.getClass().getSuperclass());
+		System.out.println(soft.toString());
+		
+		Product ram = new RamMemory("ss","asda","sdas");
+		
 	}
 
 }
